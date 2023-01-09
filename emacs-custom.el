88aaf3d9bf674c71edd243a1af9817a3c237a835
Cory Koch <yunusabd786@gmail.com>
2023-01-09 10:48:19 -0500

    Move submodules to site-lisp and misc customizations
    Installed ivy but will likely go with icomplete or fido for now.
    Cleaned up ;;; headings in init to better utilize occur

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,9 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-enabled-themes '(modus-vivendi))
+ '(display-time-mode t)
  '(package-selected-packages
-   '(sharper web-mode use-package dap-mode lsp-treemacs lsp-ui company yasnippet csharp-mode go-mode lsp-mode magit markdown-mode powershell)))
+   '(ivy sharper web-mode use-package dap-mode lsp-treemacs lsp-ui company yasnippet go-mode lsp-mode magit markdown-mode powershell))
+ '(save-place-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
